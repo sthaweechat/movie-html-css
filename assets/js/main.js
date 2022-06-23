6873d301a14b437574aaaa003a6aa15efa166a53
xou <sthaweechat@gmail.com>
2022-06-23 17:59:16 +0700

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,9 @@ arrows.forEach((arrow, i) => {
   let clickCounter = 0;
   // console.log(arrow)
   arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
-    if (itemNumber - (4 + clickCounter) >= 0) {
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
       // console.log("click " + i);
       movieLists[i].style.transform = `translateX(${
         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
@@ -21,8 +22,22 @@ arrows.forEach((arrow, i) => {
     }
   });
 
+  // console.log(window.innerWidth);
+
   // console.log(movieLists[i].computedStyleMap().get("transform")[0].x.value);
 
   // console.log(movieLists[i].querySelectorAll("img").length);
 });
 
+//toggle
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach(item => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
